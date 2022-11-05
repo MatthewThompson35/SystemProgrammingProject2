1d33551d86cdd55f7a4a469a88ce98958f12b3ff
mthomp35 <mthomp35@cs3280-vm14.uwg.westga.edu>
2022-11-04 21:47:07 -0400

    fixed pylint errors on server

--- server.py
+++ server.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
+"""Server module."""
 import socket
 import threading
 import logging
 
-"""
-To run tests server must be running. testing must be set to true otherwise testing must be set to false
-"""
 HEADER = 64
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
@@ -17,85 +15,82 @@ SOFTWARE = "QA"
 DATABASE = "DB"
 
 
-python_notes = []
-software_notes = []
-database_notes = []
+PYTHON_NOTES = []
+SOFTWARE_NOTES = []
+DATABASE_NOTES = []
 
-sending = False
-testing = False
+TESTING = False
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
+
+
+
 def handle_client(conn, addr):
+    """ Handles all of the mesages that the client sends to the server"""
     print(f"[NEW CONNECTION] {addr} connected.")
-    logging.info("Client connected at: " + addr[0])
+    ip_address = str(addr[0])
+    logging.info("Client connected at: %s", ip_address)
     connected_channel = ""
     connected = True
     writing = False
     while connected:
-            if(testing):
-                msg = DISCONNECT_MESSAGE
-            msg_length = conn.recv(HEADER).decode(FORMAT)
-            if msg_length:
-                msg_length = int(msg_length)
-                msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-            if msg in (PYTHON,SOFTWARE,DATABASE,DISCONNECT_MESSAGE):
-                if msg == PYTHON:
-                    connected_channel = "python"
-                if msg == SOFTWARE:
-                    connected_channel = "software"
-                if msg == DATABASE:
-                    connected_channel = "database"
+        if TESTING:
+            msg = DISCONNECT_MESSAGE
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+        if msg == DISCONNECT_MESSAGE:
+            connected = False
+        elif msg in (PYTHON, SOFTWARE, DATABASE, DISCONNECT_MESSAGE):
+            if msg == PYTHON:
+                connected_channel = "python"
+            if msg == SOFTWARE:
+                connected_channel = "software"
+            if msg == DATABASE:
+                connected_channel = "database"
+        elif writing:
+            if connected_channel == "python":
+                result = str(addr)
+                result += " " + msg
+                PYTHON_NOTES.append(result)
+            if connected_channel == "software":
+                result = str(addr)
+                result += " " + msg
+                SOFTWARE_NOTES.append(result)
+            if connected_channel == "database":
+                result = str(addr)
+                result += " " + msg
+                DATABASE_NOTES.append(result)
+            writing = False
+        elif msg == "WRIT":
+            writing = True
+        elif msg == "READ":
+            conn.send(get_all_notes_to_string(connected_channel).encode(FORMAT))
 
-            if(writing == True):
-                if (connected_channel == "python"):
-                    result = str(addr)
-                    result += " " + msg
-                    python_notes.append(result)
-                if (connected_channel == "software"):
-                    result = str(addr)
-                    result += " " + msg
-                    software_notes.append(result)
-                if(connected_channel == "database"):
-                    result = str(addr)
-                    result += " " + msg
-                    database_notes.append(result)
-                writing = False
+        conn.send("WHAT".encode(FORMAT))
+        print(f"[{addr}] {msg}")
 
-            if msg == "WRIT":
-                writing = True
-
-            if msg == "READ":
-                conn.send(getAllNotesToString(connected_channel).encode(FORMAT))
-
-            conn.send("WHAT".encode(FORMAT))
-            print(f"[{addr}] {msg}")
-
-    logging.info("Client disconnected at: " + addr[0])
+    logging.info("Client disconnected at: %s", ip_address)
     conn.close()
 
-def getAllNotesToString(connected_channel):
-    result = ""
-    if (connected_channel == "python"):
-        result = '\n'.join(python_notes)
-    if (connected_channel == "software"):
-        result = '\n'.join(software_notes)
-    if(connected_channel == "database"):
-        result = '\n'.join(database_notes)
+
+def get_all_notes_to_string(connected_channel):
+    """Gets all of the notes that are in the correlated channel """
+    if connected_channel == "python":
+        result = '\n'.join(PYTHON_NOTES)
+    if connected_channel == "software":
+        result = '\n'.join(SOFTWARE_NOTES)
+    if connected_channel == "database":
+        result = '\n'.join(DATABASE_NOTES)
     return result + "\n"
 
-def addNoteToAssociatedChannel(msg):
-    if (connected_channel == "python"):
-        python_notes.append(msg)
-    if (connected_channel == "software"):
-        software_notes.append(msg)
-    if(connected_channel == "database"):
-        database_notes.append(msg)
+
 
 def start():
+    """Starts the server"""
     server.listen()
     print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
@@ -104,7 +99,10 @@ def start():
         thread.start()
         print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
 
+
+
 def main():
+    """The main function"""
     logging.basicConfig(filename='serverlog.log', level=logging.INFO)
     print("[STARTING] server is starting...")
     start()
